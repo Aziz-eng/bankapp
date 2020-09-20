f9f3dc63d56c7184bb153f4d8d61f01ffa1a8216
Aziz.Huseynov <huseynov.aziz@gmail.com>
2020-09-20 23:54:11 +0500

    Папку смд тоже добавил

---
+++ cmd/bank/main.go
@@ -0,0 +1,21 @@
+package main
+import(
+	// "bank/pkg/bank/types"
+	// "fmt"
+)
+
+
+
+func main()  {
+	// cards:= []types.Card {
+	// 	Balance: 100000,
+	// 	Active: true,
+	// }
+	// fmt.Println (cards)
+		
+		
+
+	
+	
+	
+}
