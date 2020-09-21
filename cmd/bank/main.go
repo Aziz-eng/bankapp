8e6d971a831a26344c49b47952476e1138ab037a
Aziz.Huseynov <huseynov.aziz@gmail.com>
2020-09-21 11:07:19 +0500

    Написал код main.go для дебагера

--- cmd/bank/main.go
+++ cmd/bank/main.go
@@ -1,21 +1,34 @@
 package main
-import(
-	// "bank/pkg/bank/types"
-	// "fmt"
+
+import (
+	"bank/pkg/bank/card"
+	"bank/pkg/bank/types"
+	"fmt"
 )
 
-
-
-func main()  {
+func main() {
 	// cards:= []types.Card {
 	// 	Balance: 100000,
 	// 	Active: true,
 	// }
 	// fmt.Println (cards)
-		
-		
+	cards := []types.Card{
+		// {
 
-	
-	
-	
+		// 	PAN:     string,
+		// 	Balance: Money,
+		// 	Active:  true,
+		// },
+		{
+
+			PAN:     "2525 xxxx xxxx 0002",
+			Balance: 25000,
+			Active:  true,
+		},
+	}
+	paymentSources := card.PaymentSources(cards)
+	for _, v := range paymentSources {
+		fmt.Println(v.Number)
+	}
+
 }
